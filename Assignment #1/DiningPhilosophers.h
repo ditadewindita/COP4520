131ea67005842c54922d2a4a2ed7725b28ca3707
Dita Dewindita <dita.dewindita@gmail.com>
2018-01-22 01:55:03 -0500

    Setup objects and initialization

---
+++ Assignment #1/DiningPhilosophers.h
@@ -0,0 +1,42 @@
+#ifndef DININGPHILOSOPHERS_H
+#define DININGPHILOSOPHERS_H
+
+#include <thread>
+#include <mutex>
+
+// Philosopher states
+#define THINKING 0
+#define EATING 1
+#define WAITING 2
+
+using namespace std;
+
+class Chopstick {
+  public:
+    int id;
+    mutex *chopstick;
+
+    Chopstick(int id, mutex *chopstick);
+    
+    void putDown();
+
+    void pickUp();
+};
+
+class Philosopher {
+  public:
+    int id;
+    int state;
+    Chopstick *left_chopstick;
+    Chopstick *right_chopstick;
+
+    Philosopher(int id, int state, Chopstick *left_chopstick, Chopstick *right_chopstick);
+
+    void think();
+
+    void eat();
+
+    void wait();
+};
+
+#endif
--- Assignment #1/DiningPhilosophersV1.cpp
+++ Assignment #1/DiningPhilosophersV1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
 #include <string>
+#include <vector>
+#include "DiningPhilosophers.h"
 
 #define NUM_PHILOSOPHERS 5
 #define NUM_CHOPSTICKS 5
-
-using namespace std;
+#define WAIT_TIME 100
 
 // NOTES:
 // Method to make program wait - for eating/thinking 'a while'
@@ -18,13 +20,60 @@ using namespace std;
 //  others eat
 // Each philosopher eats, pickup chopstick, thinks, put down chopsticks
 
+// think -> eat -> wait
+
+// CHOPSTICK DEFINITIONS
+Chopstick::Chopstick(int temp_id, mutex *temp_chopstick) : id(temp_id), chopstick(temp_chopstick) {}
+
+void Chopstick::putDown() {
+  chopstick->unlock();
+}
+
+void Chopstick::pickUp() {
+  chopstick->lock();
+}
+
+// PHILOSOPHER DEFINITIONS
+Philosopher::Philosopher(int temp_id, int temp_state, Chopstick *temp_left_chopstick, Chopstick *temp_right_chopstick) :
+  id(temp_id), state(temp_state), left_chopstick(temp_left_chopstick), right_chopstick(temp_right_chopstick) {}
+
+void Philosopher::think() {
+  cout << "Philosopher " << id << " is thinking.\n";
+  state = THINKING;
+  left_chopstick->putDown();
+  right_chopstick->putDown();
+  wait();
+}
+
+void Philosopher::eat() {
+  left_chopstick->pickUp();
+  right_chopstick->pickUp();
+  state = EATING;
+  cout << "Philosopher " << id << " is eating.\n";
+}
+
+void Philosopher::wait() {
+  state = WAITING;
+  this_thread::sleep_for(chrono::milliseconds(WAIT_TIME));
+}
 
 int main(int argc, const char *argv[]) {
   string stop = "";
-  thread *philosophers = new thread[NUM_PHILOSOPHERS];
-  mutex *chopsticks = new mutex[NUM_CHOPSTICKS];
+  vector<Philosopher*> philosophers;
+  vector<Chopstick*> chopsticks;
+
+  // Initialize chopsticks
+  for(int i = 0; i < NUM_CHOPSTICKS; i++) {
+    chopsticks[i] = new Chopstick(i, new mutex());
+  }
+
+  // Initialize philosophers and assign them their neighboring chopsticks
+  for(int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    philosophers[i] = new Philosopher(i, THINKING, chopsticks[i], chopsticks[(i + 1) % NUM_PHILOSOPHERS]);
+  }
 
   while(stop.compare("n") != 0) {
 
+    cin >> stop;
   }
 }
