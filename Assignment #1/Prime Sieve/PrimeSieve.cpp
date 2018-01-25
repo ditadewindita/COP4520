9bb652543e06f777db827a314e558882fbf80575
Dita Dewindita <dita.dewindita@gmail.com>
2018-01-24 23:02:59 -0500

    Changed to file output

--- Assignment #1/Prime Sieve/PrimeSieve.cpp
+++ Assignment #1/Prime Sieve/PrimeSieve.cpp
@@ -3,6 +3,7 @@
 // HA990936
 
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <vector>
 #include <math.h>
@@ -13,16 +14,6 @@ using namespace std;
 #define MAX_PRIME 100000000
 #define NUM_TOP_PRIMES 10
 
-// 10^8
-// 1 - 10^4 = found
-// 10 ^ 4 - 10 ^ 5
-// 10 ^ 5 - 10 ^ 6
-// 10 ^ 6 - 10 ^ 7
-// 10 ^ 7 - 10 ^ 8
-
-// 8 threads, sieve only 10 times each
-
-// O(n log*n)
 void normal_sieve(bool *primes, int n) {
 
   for(int i = 2; i * i <= n; i++)
@@ -47,33 +38,27 @@ void segmented_sieve(bool *primes, int min, int max) {
 }
 
 int main() {
-  // seq: time - 1,907,027, 1,854,579; primes - 5761455
-  //             9,645,240
-  // par: time - 9,701,474
-  //            10,023,587
-  //             9,644,667
-  //      avg -  9,348,849
-  // sum: 279209790387276
-  //      279209790387276
-
   int min = 1;
   int max = min * 10;
 
   bool *primes = new bool[MAX_PRIME + 1];
   vector<thread> threads_list;
+  ofstream f_out;
 
+  // Set the initial values of the primes array
   memset(primes, true, MAX_PRIME + 1);
   primes[0] = false;
   primes[1] = false;
+
+  f_out.open("primes.txt");
 
   long prime_sum = 0;
   int num_primes = 0;
   long *topPrimes = new long[NUM_TOP_PRIMES];
 
-  std::cout << "Launched from main.\n";
-
   int start = clock();
 
+  // Every time a new thread is spawned, shift the range of primes to find
   for(int i = 0; i < NUM_THREADS && max <= MAX_PRIME; i++){
     threads_list.push_back(thread(segmented_sieve, primes, min, max));
     min = max + 1;
@@ -83,8 +68,6 @@ int main() {
   for_each(threads_list.begin(), threads_list.end(), mem_fn(&thread::join));
 
   int stop = clock();
-
-  std::cout << "Joined all threads.\n";
 
   int numMaxPrimes = 0;
 
@@ -97,17 +80,18 @@ int main() {
         topPrimes[numMaxPrimes++] = i;
     }
 
-  std::cout << (double)(stop - start) / CLOCKS_PER_SEC << "s, num_primes: " << num_primes << ", prime_sum: " << prime_sum << "\n";
-  std::cout << "Top " << NUM_TOP_PRIMES << " primes: ";
+  f_out << (double)(stop - start) / CLOCKS_PER_SEC << "s " << num_primes << " " << prime_sum << "\n";
 
   for(int i = 0; i < NUM_TOP_PRIMES; i++) {
-    std::cout << topPrimes[i];
+    f_out << topPrimes[i];
 
     if(i == NUM_TOP_PRIMES - 1)
-      std::cout << "\n";
+      f_out << "\n";
     else
-      std::cout << ", ";
+      f_out << ", ";
   }
+
+  f_out.close();
 
   return 0;
 }
