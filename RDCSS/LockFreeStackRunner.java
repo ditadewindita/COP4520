2d06adf070e053252fd4010891da1b40674d6820
Dita Dewindita <dita.dewindita@gmail.com>
2018-03-19 23:32:32 -0400

    RDCSS

---
+++ RDCSS/LockFreeStackRunner.java
@@ -0,0 +1,126 @@
+// Haerunnisa Dewindita
+// COP4520, Spring 2018
+// HA990936
+
+import java.util.concurrent.locks.*;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.*;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+// Current Runnable implements the stack to hold INTEGERS and randomizes the data
+class LockFreeRunnable implements Runnable {
+  private static final int PUSH_OP = 0;
+  private static final int POP_OP = 1;
+  private static final int SIZE_OP = 2;
+  private final int id;
+  // private int currOp;
+  public AtomicReference<LockFreeStack<Integer>> stack;
+
+  public LockFreeRunnable(LockFreeStack<Integer> stack, int id) {
+    this.stack = new AtomicReference<>(stack);
+    this.id = id;
+    // this.currOp = currOp;
+  }
+
+  public void run() {
+    int operation = (int)(Math.random() * 3);
+
+    if(operation == PUSH_OP) {
+      int randomNum = LockFreeStackRunner.getRandomNumber();
+      boolean push = stack.get().push(randomNum);
+      long time = System.currentTimeMillis();
+
+      if(push)
+        System.out.println("-> Thread " + this.id + " pushed " + randomNum + " at " + time + "ms.");
+      else
+        System.out.println("-> Thread " + this.id + " failed call to push() " + " at " + time + "ms.");
+    }
+    else if(operation == POP_OP){
+      Integer pop = stack.get().pop();
+      long time = System.currentTimeMillis();
+
+      if(pop != null)
+        System.out.println("-> Thread " + this.id + " popped " + pop + " at " + time + "ms.");
+      else
+        System.out.println("-> Thread " + this.id + " failed call to pop()" + " at " + time + "ms.");
+    }
+    else {
+      int size = stack.get().size();
+      long time = System.currentTimeMillis();
+
+      System.out.println("-> Thread " + this.id + " checked size as " + size + " at " + time + "ms.");
+    }
+  }
+}
+
+public class LockFreeStackRunner<T> {
+  public static final int MIN_NUM = 0;
+  public static final int MAX_NUM = 300;
+  public static final int NUM_THREADS = 10;
+  public static final int MAX_NUM_NODES = 1000;
+
+  public static int getRandomNumber() {
+    return ThreadLocalRandom.current().nextInt(MIN_NUM, MAX_NUM);
+  }
+
+  public static void main(String[] args) throws Exception {
+    Thread threads[] = new Thread[NUM_THREADS];
+    LockFreeStack<Integer> lockFreeStack = new LockFreeStack<>();
+    // int currNumThreads = 1;
+
+    // // Filenames
+    // String files[] = {"push.csv", "pop.csv", "size.csv"};
+    //
+    // for(int k = 0; k < files.length; k++) {
+    //   // Create a new file if needed
+    //   File newFile = new File(files[k]);
+    //   newFile.createNewFile();
+    //
+    //   // Set headers
+    //   PrintWriter pw = new PrintWriter(newFile);
+    //   StringBuilder sb = new StringBuilder();
+    //   sb.append("numThreads");
+    //   sb.append(',');
+    //   sb.append("executionTime");
+    //   sb.append('\n');
+    //
+    //   // If it is a pop or size operation, preload the stack with nodes
+    //   if(k == 1 || k == 2) {
+    //     for(int j = 0; j < MAX_NUM_NODES; j++) {
+    //       lockFreeStack.push(getRandomNumber());
+    //     }
+    //   }
+    //
+    //   for(int j = currNumThreads; j <= NUM_THREADS; j *= 2) {
+        long start = System.currentTimeMillis();
+
+        for(int i = 0; i < NUM_THREADS; i++) {
+          threads[i] = new Thread(new LockFreeRunnable(lockFreeStack, i));
+          threads[i].start();
+        }
+
+        for(int i = 0; i < NUM_THREADS; i++) {
+          threads[i].join();
+        }
+
+        long stop = System.currentTimeMillis();
+
+    //     sb.append(j);
+    //     sb.append(',');
+    //     sb.append((double)(stop - start) / 1000000.0);
+    //     sb.append('\n');
+    //   }
+    //
+    //   pw.write(sb.toString());
+    //   pw.close();
+    //   System.out.println("Finished writing " + files[k]);
+    // }
+
+    System.out.println();
+    lockFreeStack.printStack();
+
+    System.out.println("\nRuntime: " + (stop - start) + "ms.");
+  }
+}
---
+++ RDCSS/README.md
@@ -0,0 +1,41 @@
+# Lock-Free Stack w/ RDCSS
+
+A Lock-Free Stack implementation using Java's Atomics library. This stack uses
+AtomicInteger and AtomicReference in place of mutexes, as well as RDCSS (Restricted Double
+Compare Single Swap)
+
+## Structure
+Use the following to run the program:
+```
+$ javac LockFreeStack.java LockFreeStackRunner.java
+$ java LockFreeStackRunner
+```
+
+## RDCSS
+The RDCSS here only swaps values if both the original and expected value of address
+1 equal and the original and expected of address 2. In that case, the memory location
+in address 2 is updated to the new value wanted for address 2.
+Using Descriptor objects, the RDCSS is performed in two parts: a compare and swap
+to see if a descriptor object is still in play, and if it is, complete the remainder
+of the CAS operation. Continue to do so until CAS() stops returning descriptors.
+
+## RDCSSRead
+RDCSSRead looks at a memory location and sees if it is an RDCSSDescriptor. If it is,
+then complete its pending CAS operation and continue to check if its a descriptor.
+
+#### Correctness
+Linearizability is a correctness condition in which concurrent operations appear
+to execute instantaneously, and therefore defines that concurrent histories have some legal sequential history. This lock-free stack uses CAS operations that
+feature changes atomically. Those atomic variables within the stack only change
+if another thread does not modify the same memory location it's handling, therefore
+the method call is guaranteed to fail and not produce ambiguous results.
+
+#### Linearization Points
+Linearization points can be identified by statements whose results can be visible
+by other threads after its execution. Since the stack utilizes descriptors to modify its
+components, the linearization points are whenever a CAS operation succeeds on a
+descriptor object in `push()`, `pop()`, and `size()`.
+
+#### Progress
+This stack's use of CAS means that its operations are executed in a predictable
+manner, which satisfies linearizability's non-blocking property. By not having unexpected delays, some thread `x` does not block another thread `y` from making progress.
