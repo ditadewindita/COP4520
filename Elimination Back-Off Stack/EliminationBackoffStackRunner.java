79e22500ba4e898c0a24ae07c6a4e56b132ed914
Dita Dewindita <dita.dewindita@gmail.com>
2018-04-04 22:35:26 -0400

    Finished ElimBackoffStack

--- Elimination Back-Off Stack/EliminationBackoffStackRunner.java
+++ Elimination Back-Off Stack/EliminationBackoffStackRunner.java
@@ -15,19 +15,19 @@ class LockFreeRunnable implements Runnable {
   public static final int POP_OP = 1;
   public static final int SIZE_OP = 2;
   private final int id;
-  private int currOp;
+  // private int currOp;
   public AtomicReference<LockFreeStack<Integer>> stack;
 
-  public LockFreeRunnable(LockFreeStack<Integer> stack, int id, int currOp) {
+  public LockFreeRunnable(LockFreeStack<Integer> stack, int id) {
     this.stack = new AtomicReference<>(stack);
     this.id = id;
-    this.currOp = currOp;
+    // this.currOp = currOp;
   }
 
   public void run() {
-    // int operation = (int)(Math.random() * 3);
+    int operation = (int)(Math.random() * 3);
 
-    if(currOp == PUSH_OP) {
+    if(operation == PUSH_OP) {
       int randomNum = EliminationBackoffStackRunner.getRandomNumber();
       boolean push;
       long time;
@@ -36,18 +36,18 @@ class LockFreeRunnable implements Runnable {
         push = stack.get().push(randomNum);
         time = System.currentTimeMillis();
 
-        // if(push)
-        //   System.out.println("-> Thread " + this.id + " pushed " + randomNum + " at " + time + "ms.");
-        // else
-        //   System.out.println("-> Thread " + this.id + " failed call to push() " + " at " + time + "ms.");
+        if(push)
+          System.out.println("-> Thread " + this.id + " pushed " + randomNum + " at " + time + "ms.");
+        else
+          System.out.println("-> Thread " + this.id + " failed call to push() " + " at " + time + "ms.");
 
       } catch(Exception ex) {
         time = System.currentTimeMillis();
 
-        // System.out.println("-> Thread " + this.id + " failed call to push() " + " at " + time + "ms.");
+        System.out.println("-> Thread " + this.id + " failed call to push() " + " at " + time + "ms.");
       }
     }
-    else if (currOp == POP_OP){
+    else if (operation == POP_OP){
       Integer pop;
       long time;
 
@@ -55,15 +55,15 @@ class LockFreeRunnable implements Runnable {
         pop = stack.get().pop();
         time = System.currentTimeMillis();
 
-        // if(pop != null)
-        //   System.out.println("-> Thread " + this.id + " popped " + pop + " at " + time + "ms.");
-        // else
-        //   System.out.println("-> Thread " + this.id + " failed call to pop()" + " at " + time + "ms.");
+        if(pop != null)
+          System.out.println("-> Thread " + this.id + " popped " + pop + " at " + time + "ms.");
+        else
+          System.out.println("-> Thread " + this.id + " failed call to pop()" + " at " + time + "ms.");
 
       } catch(Exception ex) {
         time = System.currentTimeMillis();
 
-        // System.out.println("-> Thread " + this.id + " failed call to pop()" + " at " + time + "ms.");
+        System.out.println("-> Thread " + this.id + " failed call to pop()" + " at " + time + "ms.");
       }
 
     }
@@ -71,7 +71,7 @@ class LockFreeRunnable implements Runnable {
       int size = stack.get().size();
       long time = System.currentTimeMillis();
 
-      // System.out.println("-> Thread " + this.id + " checked size as " + size + " at " + time + "ms.");
+      System.out.println("-> Thread " + this.id + " checked size as " + size + " at " + time + "ms.");
     }
   }
 }
@@ -81,85 +81,111 @@ public class EliminationBackoffStackRunner<T> {
   public static final int MAX_NUM = 300;
   public static final int NUM_THREADS = 32;
   public static final int MAX_NUM_NODES = 1000;
+  public static final int LOCK_FREE_STACK = 0;
   public static final int ELIM_BACKOFF_STACK = 1;
-  public static final int LOCK_FREE_STACK = 2;
+  public static final int NUM_RUNS = 50;
 
   public static int getRandomNumber() {
     return ThreadLocalRandom.current().nextInt(MIN_NUM, MAX_NUM);
   }
 
-  public static StringBuilder runLockFreeStack(Thread[] threads, LockFreeStack<Integer> stack, StringBuilder sb, int currOp, int stackType) {
-    // If it is a pop or size operation, preload the stack with nodes
-    if(currOp == LockFreeRunnable.POP_OP || currOp == LockFreeRunnable.SIZE_OP) {
-      for(int j = 0; j < MAX_NUM_NODES; j++) {
-        stack.push(getRandomNumber());
-      }
-    }
-
-    for(int j = 1; j <= NUM_THREADS; j *= 2) {
-      long start = System.nanoTime();
-
-      for(int i = 0; i < j; i++) {
-        threads[i] = new Thread(new LockFreeRunnable(stack, i, currOp));
-        threads[i].start();
-      }
-
-      for(int i = 0; i < j; i++) {
-        try {
-          threads[i].join();
-        } catch(Exception ex) {
-
-        }
-      }
-
-      long stop = System.nanoTime();
-
-      sb.append(j);
-      sb.append(',');
-      sb.append((double)(stop - start) / 1000000.0);
-      sb.append(',');
-      sb.append(stackType);
-      sb.append('\n');
-    }
-
-    return sb;
-  }
+  // Used for when creating CSV files to build performance graphs
+  // public static StringBuilder runLockFreeStack(Thread[] threads, LockFreeStack<Integer> stack, StringBuilder sb, int currOp, int stackType) {
+  //   // If it is a pop or size operation, preload the stack with nodes
+  //   if(currOp == LockFreeRunnable.POP_OP || currOp == LockFreeRunnable.SIZE_OP) {
+  //     for(int j = 0; j < MAX_NUM_NODES; j++) {
+  //       stack.push(getRandomNumber());
+  //     }
+  //   }
+  //
+  //   for(int j = 1; j <= NUM_THREADS; j *= 2) {
+  //     double totalTime = 0;
+  //
+  //     for(int l = 0; l < NUM_RUNS; l++) {
+  //       long start = System.nanoTime();
+  //
+  //       for(int i = 0; i < j; i++) {
+  //         threads[i] = new Thread(new LockFreeRunnable(stack, i, currOp));
+  //         threads[i].start();
+  //       }
+  //
+  //       for(int i = 0; i < j; i++) {
+  //         try {
+  //           threads[i].join();
+  //         } catch(Exception ex) {
+  //
+  //         }
+  //       }
+  //
+  //       long stop = System.nanoTime();
+  //
+  //       totalTime += (double)(stop - start) / 1000000.0;
+  //     }
+  //
+  //     sb.append(j);
+  //     sb.append(',');
+  //     sb.append(totalTime / NUM_RUNS);
+  //     sb.append(',');
+  //     sb.append(stackType);
+  //     sb.append('\n');
+  //   }
+  //
+  //   return sb;
+  // }
 
   public static void main(String[] args) throws Exception {
-    Thread threadsElim[] = new Thread[NUM_THREADS];
-    Thread threadsLockFree[] = new Thread[NUM_THREADS];
+    Thread threads[] = new Thread[NUM_THREADS];
+    // Thread threadsLockFree[] = new Thread[NUM_THREADS];
     LockFreeStack<Integer> eliminationBackoffStack = new EliminationBackoffStack<>();
-    LockFreeStack<Integer> lockFreeStack = new LockFreeStack<>();
+    // LockFreeStack<Integer> lockFreeStack = new LockFreeStack<>();
 
     // Filenames
-    String files[] = {"push.csv", "pop.csv", "size.csv"};
-
-    for(int k = 0; k < files.length; k++) {
-      // Create a new file if needed
-      File newFile = new File(files[k]);
-      newFile.createNewFile();
-
-      // Set headers
-      PrintWriter pw = new PrintWriter(newFile);
-      StringBuilder sb = new StringBuilder();
-      sb.append("numThreads");
-      sb.append(',');
-      sb.append("executionTime");
-      sb.append(',');
-      sb.append("stackType");
-      sb.append('\n');
-
-      sb = runLockFreeStack(threadsLockFree, lockFreeStack, sb, k, LOCK_FREE_STACK);
-      sb = runLockFreeStack(threadsElim, eliminationBackoffStack, sb, k, ELIM_BACKOFF_STACK);
-
-      pw.write(sb.toString());
-      pw.close();
-      System.out.println("Finished writing " + files[k]);
-    }
+    // String files[] = {"push.csv", "pop.csv", "size.csv"};
     //
-    // System.out.println();
-    // eliminationBackoffStack.printStack();
+    // for(int k = 0; k < files.length; k++) {
+    //   // Create a new file if needed
+    //   File newFile = new File(files[k]);
+    //   newFile.createNewFile();
+    //
+    //   // Set headers
+    //   PrintWriter pw = new PrintWriter(newFile);
+    //   StringBuilder sb = new StringBuilder();
+    //   sb.append("numThreads");
+    //   sb.append(',');
+    //   sb.append("executionTime");
+    //   sb.append(',');
+    //   sb.append("stackType");
+    //   sb.append('\n');
 
-    // System.out.println("\nRuntime: " + (stop - start) + "ms.");
+      // sb = runLockFreeStack(threadsLockFree, lockFreeStack, sb, k, LOCK_FREE_STACK);
+      // sb = runLockFreeStack(threadsElim, eliminationBackoffStack, sb, k, ELIM_BACKOFF_STACK);
+
+    //   pw.write(sb.toString());
+    //   pw.close();
+    //   System.out.println("Finished writing " + files[k]);
+    // }
+
+    long start = System.currentTimeMillis();
+
+    for(int i = 0; i < NUM_THREADS; i++) {
+      threads[i] = new Thread(new LockFreeRunnable(eliminationBackoffStack, i));
+      threads[i].start();
+    }
+
+    for(int i = 0; i < NUM_THREADS; i++) {
+      try {
+        threads[i].join();
+      } catch(Exception ex) {
+
+      }
+    }
+
+    long stop = System.currentTimeMillis();
+
+
+    System.out.println();
+    eliminationBackoffStack.printStack();
+
+    System.out.println("\nRuntime: " + (stop - start) + "ms.");
   }
 }
--- Elimination Back-Off Stack/dataPlotter.py
+++ Elimination Back-Off Stack/dataPlotter.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as pt
 import csv
 
-data = ["pop", "push", "size"]
-stackTypes = ["Elimination Backoff Stack", "Lockfree Stack"]
-shapes = ["o", "s", "v"]
+data = ["push", "pop", "size"]
+stackTypes = ["Lock-Free Stack", "Elimination Back-Off Stack"]
+shapes = ["o", "s"]
+figure_num = 0
 for idx in range(len(data)):
-  with open("%s.csv" % data[idx], "rb") as c:
-    a = csv.reader(c)
-    x = []
-    y = []
-    for row in a:
-      x.append(row[0])
-      y.append(row[1])
-    pt.title("Threads vs Time")
-    pt.xlabel("threads")
-    pt.ylabel("time (ms)")
-    pt.plot(x[1:], y[1:], "-%s" % shapes[idx], label=data[idx])
-pt.legend(loc = 'best')
-pt.show()
+    with open("%s.csv" % data[idx], "rb") as c:
+        a = csv.reader(c)
+        x = []
+        y = []
+        for row in a:
+            x.append(row[0])
+            y.append(row[1])
+        pt.figure(idx)
+        pt.title("Threads vs Time for %s operation" % data[idx])
+        pt.xlabel("Threads")
+        pt.ylabel("Time (ms)")
+        pt.plot(x[1:7],y[1:7], "-%s" % shapes[0], label = stackTypes[0])
+        pt.plot(x[7:], y[7:], "-%s" % shapes[1], label = stackTypes[1])
+    pt.legend(loc='best')
+    pt.show()
